1600055c15accfc866325586a68019a7d23cead2
Amy Yi Li <142811190+AmyLi12@users.noreply.github.com>
2025-03-19 16:07:39 +0000

    completed class activity

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
 export default function Card({ imgUri, title, children }) {
 
   return (
-    <div>
-      <img src={imgUri} alt="" />
+    <div className="w-xs rounded-lg bg-slate-200 p-4">
+      <img src={imgUri} alt="" className="rounded-sm"/>
 
       <div>
         <h3>{title}</h3>
